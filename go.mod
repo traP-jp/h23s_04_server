c15dddeeb5d946dc3edec877b1827b9a43a9e7f1
Nagamatsu Ryosuke <koukapoke@gmail.com>
2023-06-17 13:08:49 +0900

    go-test

---
+++ go.mod
@@ -0,0 +1,7 @@
+module server
+
+go 1.20
+
+require github.com/trevex/golem v0.0.0-20170330051645-8705b20d71c3
+
+require github.com/gorilla/websocket v1.5.0 // indirect
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	// WebSocket接続をアップグレードします
+	upgrader := websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Upgrade error:", err)
+		return
+	}
+
+	// 接続が確立されたことをクライアントに通知します
+	if err := conn.WriteMessage(websocket.TextMessage, []byte("Connected to server")); err != nil {
+		log.Println("Write error:", err)
+		return
+	}
+
+	// メッセージを読み取り、処理するループ
+	for {
+		// クライアントからメッセージを受信します
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Read error:", err)
+			break
+		}
+
+		// 受信したメッセージを処理します（ここでは単純にログに出力します）
+		log.Println("Received message:", string(message))
+
+		// クライアントにメッセージを送信します
+		if err := conn.WriteMessage(websocket.TextMessage, []byte("Message received")); err != nil {
+			log.Println("Write error:", err)
+			break
+		}
+	}
+
+	// 接続を閉じます
+	conn.Close()
+}
+
+func main() {
+	http.HandleFunc("/websocket", handleWebSocket)
+
+	// サーバーを起動します
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("Server error:", err)
+	}
+}
